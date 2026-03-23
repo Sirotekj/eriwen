673fc7cc51eb1f4d011fd51a2c3c6d0beac78291
Sirotekj <sirotek.j@gmail.com>
2026-03-23 14:19:03 +0100

    MainMenu Mobile

---
+++ components/main-menu/main-menu-mobile.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import { useState } from 'react';
+import Link from 'next/link';
+
+import { IconMenuMobile } from '@/components/utils/svgs/icons';
+
+const MainMenuMobile = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen((prev) => !prev);
+  const closeMenu = () => setIsOpen(false);
+
+  return (
+    <>
+      <button
+        title="Menu"
+        className="flex mt-1 ml-1 mr-2 w-8 h-8 cursor-pointer md:hidden"
+        onClick={toggleMenu}
+      >
+        <IconMenuMobile className="w-full h-full" />
+      </button>
+      {isOpen && (
+        <div className="fixed inset-0 bg-black/50 z-40" onClick={closeMenu} />
+      )}
+      <div
+        className={`fixed top-0 right-0 h-full w-64 bg-background z-50 transform transition-transform duration-300 ${
+          isOpen ? 'translate-x-0' : 'translate-x-full'
+        }`}
+      >
+        <div className="p-4 flex justify-end cursor-pointer">
+          <button onClick={closeMenu}>✕</button>
+        </div>
+
+        <ul className="flex flex-col gap-4 p-4 text-lg">
+          <li>
+            <Link href="/svet" onClick={closeMenu}>
+              Svět
+            </Link>
+            <ul className="ml-4 text-base list-disc">
+              <li className="ml-1">
+                <Link href="/svet/kraje-a-mista">Kraje a místa</Link>
+              </li>
+              <li className="ml-1">
+                <Link href="/svet/nabozenstvi">Náboženství</Link>
+              </li>
+              <li className="ml-1">
+                <Link href="/svet/spojenci">Spojenci</Link>
+              </li>
+              <li className="ml-1">
+                <Link href="/svet/nepratele">Nepřátelé</Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link href="/postavy" onClick={closeMenu}>
+              Postavy
+            </Link>
+          </li>
+          <li>
+            <Link href="/tazeni" onClick={closeMenu}>
+              Tažení
+            </Link>
+          </li>
+          <li>
+            <Link href="/bestiar" onClick={closeMenu}>
+              Bestiář
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+export default MainMenuMobile;
--- components/utils/svgs/icons.tsx
+++ components/utils/svgs/icons.tsx
@@ -38,3 +38,13 @@ export function IconDelete(props: React.SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function IconMenuMobile(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
+      <path d="M5 6.5H19V8H5V6.5Z" fill="currentColor" />
+      <path d="M5 16.5H19V18H5V16.5Z" fill="currentColor" />
+      <path d="M5 11.5H19V13H5V11.5Z" fill="currentColor" />
+    </svg>
+  );
+}
