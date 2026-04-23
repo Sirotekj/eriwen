6f2d43b7c68ca91322e7c17073194003e2a55dd3
Sirotekj <sirotek.j@gmail.com>
2026-04-23 18:53:59 +0200

    postavy - poznamka

--- app/postavy/page.tsx
+++ app/postavy/page.tsx
@@ -32,45 +32,18 @@ export default async function PostavyPage({ searchParams }: PageProps) {
       <p>
         Zde můžete najít stručný popis postav a jakých tažení se zúčastnili.
       </p>
-      {isEditing && canCreate && <PostavyCreateToggle />}
+      {isEditing && canCreate && (
+        <>
+          <PostavyCreateToggle />
+          <p>Pozn.: Postavy se řadí abecedně.</p>
+        </>
+      )}
       <PostavyList
         postavy={postavy}
         role={role}
         userId={userId}
         isEditing={isEditing}
       />
-
-      {/*<ul>
-        <li>
-          <strong>Bered</strong> - trpaslík (válečník)
-          <ImageWrapper>
-            <Image
-              className="object-cover"
-              src="/images/bered.jpg"
-              alt="bered"
-              fill
-            />
-          </ImageWrapper>
-          <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent
-            id justo in neque elementum ultrices. Integer rutrum, orci
-            vestibulum ullamcorper ultricies, lacus quam ultricies odio, vitae
-            placerat pede sem sit amet enim. Praesent dapibus. Maecenas
-            fermentum, sem in pharetra pellentesque, velit turpis volutpat ante,
-            in pharetra metus odio a lectus. Integer pellentesque quam vel
-            velit. Donec iaculis gravida nulla. Vivamus luctus egestas leo.
-            Aenean placerat. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos hymenaeos. Fusce tellus odio, dapibus
-            id fermentum quis, suscipit id erat. Maecenas lorem. Vestibulum
-            fermentum tortor id mi. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Mauris metus. Phasellus
-            enim erat, vestibulum vel, aliquam a, posuere eu, velit. Nulla non
-            lectus sed nisl molestie malesuada. Sed vel lectus. Donec odio
-            tempus molestie, porttitor ut, iaculis quis, sem.
-          </p>
-        </li>
-      </ul>*/}
     </div>
   );
 }
