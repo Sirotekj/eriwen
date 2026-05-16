07351d70803e8d5f5178a9ac455a5e6bd9d4e19c
Sirotekj <sirotek.j@gmail.com>
2026-05-16 17:43:06 +0200

    overflow paper fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,10 +42,12 @@ export default function RootLayout({
         <Background />
         <Providers>
           <MainMenu />
-          <main className="relative max-w-3xl mx-auto my-36 px-4 pt-24 pb-16">
-            <PaperBackground />
-            {children}
-          </main>
+          <div className="w-screen overflow-hidden">
+            <main className="relative max-w-3xl mx-auto my-36 px-4 pt-24 pb-16">
+              <PaperBackground />
+              {children}
+            </main>
+          </div>
         </Providers>
       </body>
     </html>
