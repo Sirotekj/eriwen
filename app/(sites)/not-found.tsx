584e805ab06fa326dc73c846d2221ecae93e4cfa
Sirotekj <sirotek.j@gmail.com>
2026-05-22 16:34:12 +0200

    not-found for (sites)

---
+++ app/(sites)/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Stránka nenalezena</h1>
+      <p>Bohužel, jsme nenašli požadovanou stránku.</p>
+    </main>
+  );
+}
