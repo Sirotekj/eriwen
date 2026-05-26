8a3e4960e5967aff021ae7f3ab7a38720d5ddd0b
Sirotekj <sirotek.j@gmail.com>
2026-05-26 11:11:28 +0200

    image wrapper update

--- components/utils/image-wrapper.tsx
+++ components/utils/image-wrapper.tsx
@@ -11,7 +11,7 @@ export default function ImageWrapper({ children, size }: ImageWrapperProps) {
         ${
           size === 'large'
             ? 'float-none w-full max-w-full aspect-auto'
-            : 'float-left max-w-xs w-full xs:w-1/2 sm:w-1/3 aspect-3/4 mr-4 mb-4'
+            : 'w-full mb-4 xs:max-w-xs xs:w-1/2 xs:float-left xs:mr-4 xs: sm:w-1/3 aspect-3/4'
         }
       `}
     >
