7e304193834235090f1c3ad387146429252cadb3
Sirotekj <sirotek.j@gmail.com>
2026-07-30 12:56:56 +0200

    paper bottom update

--- components/background/paper.tsx
+++ components/background/paper.tsx
@@ -4,7 +4,7 @@ export default function PaperBackground() {
       <div className="absolute top-0 left-[-8%] w-[116%] h-full flex flex-col z-[-1]">
         <div className="aspect-1024/152 w-full bg-cover bg-center bg-no-repeat bg-[url('/images/burnt-paper-top_2.webp')]"></div>
         <div className="relative flex-1 w-full -top-px bg-size-[100%] bg-[url('/images/burnt-paper_2.webp')]"></div>
-        <div className="absolute -bottom-8 aspect-1024/98 w-full bg-cover bg-center bg-no-repeat bg-[url('/images/burnt-paper-bottom_2.webp')]"></div>
+        <div className="absolute -bottom-4 sm:-bottom-8 aspect-1024/98 w-full bg-cover bg-center bg-no-repeat bg-[url('/images/burnt-paper-bottom_2.webp')]"></div>
       </div>
     </>
   );
